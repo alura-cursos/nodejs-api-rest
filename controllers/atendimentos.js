0e81b6be3146efdfd60304f824ba590c6b1eb3cf
Alura <alura@alura.test>
2019-09-10 17:08:55 -0300

    Final aula 2.1

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,9 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
+
+    app.post('/atendimentos', (req, res) => {
+        console.log('Atendimento enviado')
+        res.send('Post atendimento')
+    })
+     
 }
