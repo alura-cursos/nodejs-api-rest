2e281dad06dfe429f07f9131f0275a914242de03
Alura <alura@alura.test>
2019-09-12 19:12:32 -0300

    Final aula 3.4

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,17 +5,42 @@ class Atendimento {
     adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
+        
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
 
-        const sql = 'INSERT INTO Atendimentos SET ?'
-
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if(erro) {
-                res.status(400).json(erro)
-            } else {
-                res.status(201).json(resultados)
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(resultados)
+                }
+            })
+        }
+       
     }
 }
 
