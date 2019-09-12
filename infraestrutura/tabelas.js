e6dc02fc36bcb8a435fd40765179258185eba912
Alura <alura@alura.test>
2019-09-12 18:42:33 -0300

    Final aula 3.1

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -6,7 +6,7 @@ class Tabelas {
     }
 
     criarAtendimentos() {
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
 
         this.conexao.query(sql, erro => {
             if(erro) {
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,15 @@
+const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
     adiciona(atendimento) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const atendimentoDatado = {...atendimento, dataCriacao, data}
+        
         const sql = 'INSERT INTO Atendimentos SET ?'
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if(erro) {
                 console.log(erro)
             } else {
