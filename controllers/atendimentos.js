f3745cf6ac34a8dd825845383323614ad132dbf3
Ju <ju_bsnegreiros@hotmail.com>
2019-10-11 11:08:44 -0300

    Aula-4

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,32 +1,12 @@
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => {
-        Atendimento.lista(res)
-    })
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
 
-    app.get('/atendimentos/:id', (req, res) => {
-        const id = parseInt(req.params.id)
-
-        Atendimento.buscaPorId(id, res)
-    })
 
     app.post('/atendimentos', (req, res) => {
        const atendimento = req.body
 
         Atendimento.adiciona(atendimento, res)
-    }) 
-
-    app.patch('/atendimentos/:id', (req, res) => {
-        const id = parseInt(req.params.id)
-        const valores = req.body
-
-        Atendimento.altera(id, valores, res)
-    })
-
-    app.delete('/atendimentos/:id', (req, res) => {
-        const id = parseInt(req.params.id)
-
-        Atendimento.deleta(id, res)
     })
 }
